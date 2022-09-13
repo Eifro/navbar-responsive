c05b75c342bc90246dc740b558d754d4b2890160
Eifro <afrotricks19@gmail.com>
2022-09-13 13:04:11 -0500

    responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 /* fontawesome */
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCode } from "@fortawesome/free-solid-svg-icons";
+import Button from "./Button";
 
 function Navbar() {
   let links = [
@@ -19,10 +20,13 @@ function Navbar() {
           </span>
           <p>Navbar</p>
         </div>
-        <ul className="md:flex md:items-center md:gap-8">
+        <ul className="md:flex md:items-center md:gap-8 pb-8 md:pb-0 transition-all duration-500 ease-in">
           {links.map((link) => (
-            <li key={link.name}><a href={link.link} className="text-xl text-gray-800 hover:text-gray-400 duration-500">{link.name}</a></li>
+            <li key={link.name} className="my-7 md:my-0"><a href={link.link} className="text-xl text-gray-800 hover:text-gray-400 duration-500">{link.name}</a></li>
           ))}
+          <Button>
+            Get Started
+          </Button>
         </ul>
       </div>
     </div>
